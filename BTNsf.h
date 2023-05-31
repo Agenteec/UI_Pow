9210b42cf349d5064370913d25b16727a351bd9c
Agenteec <g.mens.hl@gmail.com>
2023-05-31 23:46:42 +0300

    Button

--- BTNsf.h
+++ BTNsf.h
@@ -1,8 +1,63 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-class BTNsf:public 
-{
+class Button : public sf::RectangleShape {
+public:
+    Button(float width, float height) : sf::RectangleShape(sf::Vector2f(width, height)) {
+        // Инициализация параметров кнопки
+        enabled = true;
+
+        // Задание цвета и внешнего вида кнопки
+        setFillColor(sf::Color::White);
+        setOutlineColor(sf::Color::Blue);
+        setOutlineThickness(2.0f);
+
+        // Установка текста по умолчанию
+        buttonText.setFont(buttonFont);
+        buttonText.setCharacterSize(20);
+        buttonText.setFillColor(sf::Color::Black);
+        buttonText.setString("Button");
+        buttonText.setPosition(width / 2 - buttonText.getGlobalBounds().width / 2,
+            height / 2 - buttonText.getGlobalBounds().height / 2);
+    }
 
 
+    void setFont(sf::Font& font){ buttonFont = font;}
+
+    void draw(sf::RenderWindow& window) {
+        // Отрисовка кнопки
+        window.draw(*this);
+        window.draw(buttonText);
+    }
+
+    void setEnabled(bool value) {
+        enabled = value;
+
+        if (enabled) {
+            setFillColor(sf::Color::Blue);
+        }
+        else {
+            setFillColor(sf::Color(128,128,128));
+        }
+    }
+
+    bool isEnabled() const {
+        return enabled;
+    }
+
+    void setText(const std::string& text) {
+        buttonText.setString(text);
+        buttonText.setPosition(getSize().x / 2 - buttonText.getGlobalBounds().width / 2,
+            getSize().y / 2 - buttonText.getGlobalBounds().height / 2);
+    }
+
+    void setPosition(sf::Vector2f pos)
+    {
+        this->sf::RectangleShape::setPosition(pos);
+        buttonText.setPosition(sf::Vector2f(pos.x,pos.y));
+    }
+private:
+    bool enabled;
+    sf::Font buttonFont;  
+    sf::Text buttonText;  
 };
 
--- UI_Pow.cpp
+++ UI_Pow.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Tab.h"
+#include "BTNsf.h"
 #pragma region SFMLFont
 sf::Font font;
-
 #pragma endregion
+
 void openWindow2(sf::RenderWindow &w) {
     sf::Event event;
     while (w.pollEvent(event))
@@ -25,9 +26,12 @@ int main()
         std::cout << "Unable to load font!\n";
         return EXIT_FAILURE;
     }
-    //
     sf::RenderWindow window1(sf::VideoMode(800, 600), L"UI Pow");
     sf::RenderWindow window2(sf::VideoMode(800, 600), L"Tab");
+    Button ShovTab(80,35);
+    ShovTab.setPosition(Vector2f(100,100));
+    ShovTab.setFont(font);
+
     while (window1.isOpen())
     {
 
@@ -39,9 +43,12 @@ int main()
         }
         openWindow2(window2);
         
+
         // Обновление и отрисовка содержимого окон
         window1.clear(sf::Color::White);
-        // Рисуем содержимое окна 1
+        #pragma region UI
+        ShovTab.draw(window1);
+        #pragma endregion
         window1.display();
     }
 
