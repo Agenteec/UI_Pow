573dd40c476d3d868d262082fb383eecfbd73b92
True Pelmen <mmozaeva2004@gmail.com>
2023-06-01 03:06:16 +0300

    functions work

--- UI_Pow.cpp
+++ UI_Pow.cpp
@@ -70,6 +70,7 @@ int main() {
     bool show = false;
     bool quit = false;
     int CBIndex = 0;
+    Tab tabl;
     const char* CBText[2] = {"Random","Test"};
     while (window.isOpen())
     {
@@ -99,10 +100,10 @@ int main() {
                 switch (CBIndex)
                 {
                 case 0:
-                    //Random
+                    tabl.Random_click();
                     break;
                 case 1:
-                    //Test
+                    tabl.Test_click();
                     break;
                 default:
                     break;
@@ -112,7 +113,7 @@ int main() {
         ImGui::SameLine();
             if (ImGui::Button(u8"Find", ImVec2(200.f, 50.f)))
             {
-                //showNetworkGame = true;
+                std::cout << tabl.Find_clicked() << std::endl;
             }
         //ImGui::Spacing();
         ImGui::SameLine();
